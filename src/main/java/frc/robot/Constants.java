d2244ee292e7be8d7ace8cc9e680d48c5347be2f
Alhadi Hamada Ghenniwa <didigh.2010@gmail.com>
2026-01-19 19:56:16 -0500

    added constants and configs file and updated shooter subsystem with sysid

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+
+
+public class Constants {
+    public static final class ShooterConstants {
+   
+        public static final double kP = 0.1;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double minOutput = -1;
+        public static final double maxOutput = 12;
+        // public static final double velocityFF = 0.5;
+
+        public static final int shooterMotorOneID = 1;
+        public static final int shooterMotorTwoID = 2;
+        public static final int kickerMotorID = 5;
+
+  }
+
+   public static final class IntakeConstants {
+        public static final int intakeMotorOneID = 3;
+        public static final int intakeMotorTwoID = 4;
+  }
+
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,39 +1,54 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix6.hardware.TalonFX; 
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Configs;
+
 
 public class ShooterSubsystem extends SubsystemBase {
-    private final TalonFX shooterMotor;
-    private final DutyCycleOut dutyCycle = new DutyCycleOut(0); 
-    private final VelocityVoltage voltageRequest = new VelocityVoltage(0);
-  /** Creates a new ExampleSubsystem. */
+
+  private final TalonFX shooterMotorOne;
+  private final TalonFX shooterMotorTwo;
+  private final TalonFX kickerMotorOne;
+
+  private final DutyCycleOut dutyCycle = new DutyCycleOut(0);
+  private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+
   public ShooterSubsystem() {
-    shooterMotor = new TalonFX(9);
+    shooterMotorOne = new TalonFX(ShooterConstants.shooterMotorOneID);
+    shooterMotorTwo = new TalonFX(ShooterConstants.shooterMotorTwoID);
+    kickerMotorOne = new TalonFX(ShooterConstants.kickerMotorID);
+
+    configureMotors();
+    }
+
+  private void configureMotors() {
+    shooterMotorOne.getConfigurator().apply(Configs.ShooterConfigs.shooterMotorConfig());
+    shooterMotorTwo.getConfigurator().apply(Configs.ShooterConfigs.shooterMotorConfig());
+
+    kickerMotorOne.getConfigurator().apply(Configs.ShooterConfigs.shooterMotorConfig());
+
+
   }
 
-public void ShootFuel(double speed){
-    shooterMotor.setControl(dutyCycle.withOutput(speed));
-}
 
-public void runVelocity(double rps){
-    voltageRequest.Velocity = rps;
-    shooterMotor.setControl(voltageRequest); 
-}
+    public void shootFuel(double speed) {
+      shooterMotorOne.setControl(dutyCycle.withOutput(speed));
+      shooterMotorTwo.setControl(dutyCycle.withOutput(speed));
+      kickerMotorOne.setControl(dutyCycle.withOutput(speed));
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    public void runVelocity(double rps) {
+      velocityRequest.Velocity = rps;
+      shooterMotorOne.setControl(velocityRequest);
+      shooterMotorTwo.setControl(velocityRequest);
+      kickerMotorOne.setControl(velocityRequest);
+    }
+
+  
 }
